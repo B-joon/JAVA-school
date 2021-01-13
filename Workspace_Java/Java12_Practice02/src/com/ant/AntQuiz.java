1b7fe021ba7d3a0407774179a624f25bcea9d783
kas7894@naver.com <dosel45@gmail.com>
2021-01-13 09:59:29 +0900

    Ant Sequence success

--- Workspace_Java/Java12_Practice02/src/com/ant/AntQuiz.java
+++ Workspace_Java/Java12_Practice02/src/com/ant/AntQuiz.java
@@ -1,5 +1,8 @@
 package com.ant;
 
+import java.util.Scanner;
+
+
 /*
  * 1
  * 1 1
@@ -14,4 +17,56 @@ package com.ant;
 
 public class AntQuiz {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Line : ");
+		int line = sc.nextInt();
+
+		String start = "1";
+		String next = "";
+		
+		for (int i = 0; i < line; i++) {
+			System.out.println(start);
+			next = nextLine(start);
+			start = next;
+			
+		}
+		sc.close();
+		
+	}
+	
+	public static String nextLine (String line) { // 출력
+
+		
+		String next = "";
+		
+		for (int i = 0; i < line.length(); i++) {
+			if (i > 0) {
+				if (line.charAt(i - 1) == line.charAt(i)) {
+					continue;
+				}
+			}
+			next += numberCut(line, i);
+		}
+		
+		
+		return next;
+	}
+	
+	public static String numberCut (String nLine, int index) { // 계산
+		
+		char str = nLine.charAt(index);
+		int count = 0;
+		for (int i = index; i < nLine.length(); i++) {
+			if (str == nLine.charAt(i)) {
+				count++;
+			} else {
+				break;
+			}
+		}
+		
+		return str + Integer.toString(count);
+	}
+	
 }
