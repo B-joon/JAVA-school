bcb36040edba699b71986bc93b8f157967effd11
kas7894@naver.com <dosel45@gmail.com>
2021-02-19 15:02:19 +0900

    Login Prectice

---
+++ Workspace_WEB/Jsp04_LoginBoard/src/com/login/biz/MYMemberBiz.java
@@ -0,0 +1,44 @@
+package com.login.biz;
+
+import java.util.List;
+
+import com.login.dao.MYMemberDao;
+import com.login.dto.MYMemberDto;
+
+public class MYMemberBiz {
+	
+	MYMemberDao dao;
+	
+	public MYMemberBiz() {
+		dao = new MYMemberDao();
+	}
+
+	public List<MYMemberDto> selectAllUser() {
+		return null;
+	}
+	public List<MYMemberDto> selectEnabledUser() {
+		return null;
+	}
+	public int updateRole(int myno, String myrole) {
+		return 0;
+	}
+	
+	public MYMemberDto login(String myid, String mypw) {
+		return dao.login(myid, mypw);
+	}
+	public MYMemberDto idCheck(String myid) {
+		return null;
+	}
+	public int insertUser(MYMemberDto dto) {
+		return 0;
+	}
+	public MYMemberDto selectUser(int myno) {
+		return null;
+	}
+	public int updateUser(MYMemberDto dto) {
+		return 0;
+	}
+	public int deleteUser(int myno) {
+		return 0;
+	}
+}
