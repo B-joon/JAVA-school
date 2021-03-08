07430735e17630bb54c56e1ce5dca4c63781291f
kas7894@naver.com <dosel45@gmail.com>
2021-03-08 22:11:28 +0900

    Ajax Practice

---
+++ Workspace_WEB/Jsp10_Ajax01/src/com/ajax/controller/ScoreController.java
@@ -0,0 +1,66 @@
+package com.ajax.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class ScoreController
+ */
+@WebServlet("/ScoreController")
+public class ScoreController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ScoreController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String name = request.getParameter("name");
+		int kor = Integer.parseInt(request.getParameter("kor"));
+		int eng = Integer.parseInt(request.getParameter("eng"));
+		int math = Integer.parseInt(request.getParameter("math"));
+		
+		int sum = kor + eng + math;
+		double avg = (double)sum / 3;
+		
+		// json-simple-1.1.1.jar
+		// json object를 만들자.
+		JSONObject obj = new JSONObject();
+		obj.put("name", name);
+		obj.put("sum", sum);
+		obj.put("avg", String.format("%.2f", avg));
+		// {"name":name, "sum":sum, "avg":avg}
+		
+		// toJSONString() : json 형식의 문자열로 만들자.
+		System.out.println("server에서 보내는 데이터 : " + obj.toJSONString());
+		
+		PrintWriter out = response.getWriter();
+		out.println(obj.toJSONString());
+	}
+
+}
